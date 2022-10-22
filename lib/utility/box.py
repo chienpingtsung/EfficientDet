a8ad67642752bce6165f8a26ed0d45a1c633eb17
Chienping Tsung <jianpingzong@outlook.com>
2022-10-22 11:43:52 +0800

    Update box.py

--- lib/utility/box.py
+++ lib/utility/box.py
@@ -3,20 +3,6 @@ from torch import nn
 from torchvision import ops
 
 
-class BoxDecoder(nn.Module):
-    def forward(self, regression, anchors):
-        anchors = ops.box_convert(anchors, 'xyxy', 'cxcywh')
-        anchor_cx = anchors[..., 0]
-        anchor_cy = anchors[..., 1]
-        anchor_w = anchors[..., 2]
-        anchor_h = anchors[..., 3]
-
-        cx = regression[..., 0] * anchor_w + anchor_cx
-        cy = regression[..., 1] * anchor_h + anchor_cy
-        w = torch.exp(regression[..., 2]) * anchor_w / 2
-        h = torch.exp(regression[..., 3]) * anchor_h / 2
-
-        return torch.stack([cx - w, cy - h, cx + w, cy + h], dim=1 if regression.shape == anchors.shape else 2)
 
 
 class BoxEncoder(nn.Module):
@@ -38,3 +24,19 @@ class BoxEncoder(nn.Module):
         dh = torch.log(ann_h / anchor_h)
 
         return torch.stack([dx, dy, dw, dh], dim=1)
+
+
+class BoxDecoder(nn.Module):
+    def forward(self, regression, anchors):
+        anchors = ops.box_convert(anchors, 'xyxy', 'cxcywh')
+        anchor_cx = anchors[..., 0]
+        anchor_cy = anchors[..., 1]
+        anchor_w = anchors[..., 2]
+        anchor_h = anchors[..., 3]
+
+        cx = regression[..., 0] * anchor_w + anchor_cx
+        cy = regression[..., 1] * anchor_h + anchor_cy
+        w = torch.exp(regression[..., 2]) * anchor_w / 2
+        h = torch.exp(regression[..., 3]) * anchor_h / 2
+
+        return torch.stack([cx - w, cy - h, cx + w, cy + h], dim=1 if regression.shape == anchors.shape else 2)
