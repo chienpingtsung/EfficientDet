263e22bb3ff5949264422b7bad5f2e473e78fbd5
Chienping Tsung <jianpingzong@outlook.com>
2022-10-27 15:44:12 +0800

    Update test.py

--- test.py
+++ test.py
@@ -8,4 +8,6 @@ def val(net, dataloader, criterion, device):
         with torch.no_grad():
             output = net(image.to(device))
             loss = sum(criterion(*output, boxes, classes))
+
+            epoch_loss.append(loss.item())
     return sum(epoch_loss)
