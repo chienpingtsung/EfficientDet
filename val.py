f58321546be7a75ed57def513254367e8d401dc1
Chienping Tsung <jianpingzong@outlook.com>
2022-10-30 16:59:30 +0800

    Update val.py

--- val.py
+++ val.py
@@ -7,7 +7,7 @@ def val(net, dataloader, criterion, device):
     for image, boxes, classes in dataloader:
         with torch.no_grad():
             output = net(image.to(device))
-            loss = sum(criterion(*output, boxes, classes))
+            loss = sum(criterion(*output, [b.to(device) for b in boxes], [c.to(device) for c in classes]))
 
             if torch.isfinite(loss):
                 epoch_loss.append(loss.item())
