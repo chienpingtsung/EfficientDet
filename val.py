592082b1e5038a088a8b5a4af12f2598827fbf48
Chienping Tsung <jianpingzong@outlook.com>
2022-10-31 18:24:25 +0800

    Update val.py

--- val.py
+++ val.py
@@ -1,13 +1,17 @@
 import torch
+from tqdm import tqdm
 
 
 def val(net, dataloader, criterion, device):
     net.eval()
     epoch_loss = []
-    for image, boxes, classes in dataloader:
+    progress = tqdm(dataloader)
+    for image, boxes, classes in progress:
         with torch.no_grad():
             output = net(image.to(device))
             loss = sum(criterion(*output, [b.to(device) for b in boxes], [c.to(device) for c in classes]))
+
+            progress.set_description(f'Test loss {loss.item()}')
 
             if torch.isfinite(loss):
                 epoch_loss.append(loss.item())
